9777c9c8c90173e0d8706e183f454a5531b148ae
rbnmk <37404233+rbnmk@users.noreply.github.com>
2021-07-29 08:32:29 +0200

    Update

--- PowerShell Function App/src/HttpToSendgrid/run.ps1
+++ PowerShell Function App/src/HttpToSendgrid/run.ps1
@@ -10,7 +10,9 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 $name = $Request.Query.Name
 if (-not $name) {
     $name = $Request.Body.Name
-}
+
+
+$Subject = $Request.Query.Subject
 
 $body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
 
@@ -18,8 +20,24 @@ if ($name) {
     $body = "Hello, $name. This HTTP triggered function executed successfully."
 }
 
+Foreach ($to in $env:SendGridToEmailAddress.split(';')) {
+
+    $Parameters = @{
+        FromAddress = $env:SendGridFromAddress
+        FromName    = "PSF"
+        ToAddress   = $to
+        #ToName      = "placeholder"s
+        Subject     = 
+        Body        = $Body
+        Token       = "$env:SendGridApiKey"
+    }
+
+    Send-PSSendGridMail @Parameters
+}
+
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = $body
-})
+        StatusCode = [HttpStatusCode]::OK
+        Body       = $body
+    })
+
